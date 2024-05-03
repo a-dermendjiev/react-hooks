ae48a3b18c4b94b751231b3f289d8ef0fc10a18f
a-dermendjiev <a.dermendjiev@gmail.com>
2024-05-03 21:27:17 +0300

    Resource Type

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import {useEffect, useState} from "react";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [resourceType, setResourceType] = useState('posts')
+    const [items, setItems] = useState([])
+    console.log('Render')
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+            .then(response => response.json())
+            .then(json => setItems(json))
+    }, [resourceType]);
+    return (
+        <div className="App">
+            <button onClick={() => setResourceType('posts')}>Post</button>
+            <button onClick={() => setResourceType('users')}>Users</button>
+            <button onClick={() => setResourceType('comments')}>Comments</button>
+            <h1>{resourceType}</h1>
+            {
+                items && items.map(item => {
+                    return <pre key={item.id}>{JSON.stringify(item)}</pre>
+                })
+            }
+        </div>
+    );
 }
 
 export default App;
