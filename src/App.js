4272bd8138a5fb85da31c828ed2bd43bc7ca2eaf
a-dermendjiev <a.dermendjiev@gmail.com>
2024-05-06 22:13:33 +0300

    Successfully

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import {useEffect, useRef, useState} from "react";
 
 function App() {
     const [name, setName] = useState('')
-    const inputRef= useRef()
+    const prevName= useRef('')
 
-    function setFocus (){
-        inputRef.current.focus()
-    }
+    useEffect(() => {
+        prevName.current = name
+    }, [name]);
+
 
     return (
         <div className="App">
-            <input ref={inputRef} value={name} onChange={e => setName(e.target.value)}/>
-            <div>My name is: {name}</div>
-            <button onClick={setFocus}>Focus</button>
+            <input value={name} onChange={e => setName(e.target.value)}/>
+            <div>My name is: {name} and previous was: {prevName.current}</div>
         </div>
     );
 }
